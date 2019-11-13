7b434973ca87eedac119301e24b9084df70abc23
Kyle Lin <kylelin@google.com>
2019-11-13 11:41:12 +0800

    mojito: rootdir: Modify the cpuset setting Because the arch is similiar with SDM670, we aling the setting with bonito.
    Test: build , munaual check the settings
    Change-Id: Ica44125c8ed71a85cfd6bde77c9107bceed7b094

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -232,6 +232,13 @@ case "$soc_id" in
         done
     done
 
+    # cpuset parameters
+    echo 0-7     > /dev/cpuset/top-app/cpus
+    echo 0-5,7 > /dev/cpuset/foreground/cpus
+    echo 4-5     > /dev/cpuset/background/cpus
+    echo 2-5     > /dev/cpuset/system-background/cpus
+    echo 2-5     > /dev/cpuset/restricted/cpus
+
     # Turn off scheduler boost at the end
     echo 0 > /proc/sys/kernel/sched_boost
     ;;
@@ -352,6 +359,13 @@ case "$soc_id" in
             echo 10 > $latfloor/polling_interval
         done
     done
+
+    # cpuset parameters
+    echo 0-7     > /dev/cpuset/top-app/cpus
+    echo 0-5,7 > /dev/cpuset/foreground/cpus
+    echo 4-5     > /dev/cpuset/background/cpus
+    echo 2-5     > /dev/cpuset/system-background/cpus
+    echo 2-5     > /dev/cpuset/restricted/cpus
 
     # Turn off scheduler boost at the end
     echo 0 > /proc/sys/kernel/sched_boost
