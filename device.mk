0870e8fe39c9d4289a693544e96f2b1f40494126
Arian <arian.kulmer@web.de>
2020-11-03 00:00:12 +0100

    mojito: Use common perf component Change-Id: I5d7105b0b24a02c81b840101356b28eb39ae3eda

--- device.mk
+++ device.mk
@@ -351,6 +351,10 @@ PRODUCT_PACKAGES += \
     libqti_vndfwk_detect.vendor \
     libvndfwk_detect_jni.qti.vendor
 
+# QTI
+TARGET_COMMON_QTI_COMPONENTS := \
+    perf
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
     $(LOCAL_PATH)/configs/permissions/system_ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml
