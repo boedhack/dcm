d2bdaea9b16bc7c8f640e14c7d1991dcf9430599
boedhack99 <boedhack@gmail.com>
2021-10-06 12:05:32 +0700

    mojito: Fix adjustment path
    Signed-off-by: boedhack99 <boedhack@gmail.com>

--- p404_mojito.mk
+++ p404_mojito.mk
@@ -11,10 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mojito device
 $(call inherit-product, device/xiaomi/mojito/device.mk)
 
-# Inherit some common Havoc stuff
+# Inherit some common p404 stuff
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
-$(call inherit-product, vendor/configs/common.mk)
+$(call inherit-product, vendor/404/configs/common.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := p404_mojito
